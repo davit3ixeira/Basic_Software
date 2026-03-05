e78c95df26a9b7668fdbb0a2196777a2917ea679
Davi Teixeira <dtm.almeida@gmail.com>
2026-03-05 16:34:53 +0000

    lab2

--- inf1018/lab2/string2num.c
+++ inf1018/lab2/string2num.c
@@ -3,8 +3,14 @@
 
 int string2num (char *s, int b) {
   int a = 0;
-  for (; *s; s++)
-    a = a*isdigit(b) + (*s - '0');
+  for (; *s; s++){
+    if(isdigit(*s)){
+      a = a*b + (*s - '0');
+    }
+    else{
+      a = a*b + (*s - 'A');
+    }
+  }
   return a;
 }
 
@@ -16,5 +22,7 @@ int main (void) {
   */
   printf("%d\n", string2num("777", 8));
   printf("%d\n", string2num("777", 10));
+  printf("%d\n", string2num("1A", 16));
+  printf("%d\n", string2num("A09B", 16));
   return 0;
 }
