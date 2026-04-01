f328c48b79b16773409089f3b5bd553e8c5fc1e1
Davi Teixeira <dtm.almeida@gmail.com>
2026-04-01 11:19:09 +0000

    lab6

--- inf1018/lab6/prog2.s
+++ inf1018/lab6/prog2.s
@@ -1,25 +1,12 @@
-/*
-int nums[] = {10, -21, -30, 45};
-int main() {
-  int i, *p;
-  int sum = 0;
-  for (i = 0, p = nums; i != 4; i++, p++)
-    sum += *p;
-  printf("%d\n", sum);
-  return 0;
-}
-*/
-
 .data
 nums:  .int  10, -21, -30, 45
-Sf:  .string "%d\n"    # string de formato para printf
+Sf:  .string "%d\n"
 
 .text
 .globl  main
 main:
 
 /********************************************************/
-/* mantenha este trecho aqui e nao mexa - prologo !!!   */
   pushq   %rbp
   movq    %rsp, %rbp
   subq    $16, %rsp
@@ -29,33 +16,35 @@ main:
 
   movl  $0, %ebx  /* ebx = 0; */
   movq  $nums, %r12  /* r12 = &nums */
-  movl  $0, %r13d 
 
 L1:
   cmpl  $4, %ebx  /* if (ebx == 4) ? */
   je  L2          /* goto L2 */
 
-  movl  (%r12), %eax    /* eax = *r12 */
-  addl  %eax, %r13d
+  cmpl  $4, %ebx
+
+  movl  (%r12), %eax    /* eax = *p */
+
+  testl $1, %eax
+  jne  L3
 
 /*************************************************************/
-/* este trecho imprime o valor de %eax (estraga %eax)  */
-  movq    $Sf, %rdi    /* primeiro parametro (ponteiro)*/
-  movl    %r13d, %esi   /* segundo parametro  (inteiro) */
-  call  printf       /* chama a funcao da biblioteca */
+  movq    $Sf, %rdi
+  movl    (%r12), %esi
+  call    printf
 /*************************************************************/
 
+L3:
   addl  $1, %ebx  /* ebx += 1; */
   addq  $4, %r12  /* r12 += 4; */
 
   jmp  L1         /* goto L1; */
 
-L2:  
+L2:
 /***************************************************************/
-/* mantenha este trecho aqui e nao mexa - finalizacao!!!!      */
-  movq  $0, %rax  /* rax = 0  (valor de retorno) */
+  movq  $0, %rax
   movq  -8(%rbp), %rbx
   movq  -16(%rbp), %r12
   leave
-  ret      
+  ret
 /***************************************************************/
--- inf1018/lab6/prog3.s
+++ inf1018/lab6/prog3.s
@@ -1,17 +1,15 @@
 /*
-int nums[] = {10, -21, -30, 45};
+char nums[] = {10, -21, -30, 45};
 int main() {
   int i, *p;
-  int sum = 0;
   for (i = 0, p = nums; i != 4; i++, p++)
-    sum += *p;
-  printf("%d\n", sum);
+    printf("%c\n", *p);
   return 0;
 }
 */
 
 .data
-nums:  .int  10, -21, -30, 45
+nums:  .byte  10, -21, -30, 45
 Sf:  .string "%d\n"    # string de formato para printf
 
 .text
@@ -29,25 +27,22 @@ main:
 
   movl  $0, %ebx  /* ebx = 0; */
   movq  $nums, %r12  /* r12 = &nums */
-  movl  $0, %r13d 
 
 L1:
   cmpl  $4, %ebx  /* if (ebx == 4) ? */
   je  L2          /* goto L2 */
 
-  movl  (%r12), %eax    /* eax = *r12 */
-  addl  %eax, %r13d
+  movsbl  (%r12), %eax    /* eax = *r12 */
 
 /*************************************************************/
 /* este trecho imprime o valor de %eax (estraga %eax)  */
   movq    $Sf, %rdi    /* primeiro parametro (ponteiro)*/
-  movl    %r13d, %esi   /* segundo parametro  (inteiro) */
+  movl    %eax, %esi   /* segundo parametro  (inteiro) */
   call  printf       /* chama a funcao da biblioteca */
 /*************************************************************/
 
   addl  $1, %ebx  /* ebx += 1; */
-  addq  $4, %r12  /* r12 += 4; */
-
+  addq  $1, %r12  /* r12 += 4; */
   jmp  L1         /* goto L1; */
 
 L2:  
